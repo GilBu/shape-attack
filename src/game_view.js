4dfe0bdc526d16222c54f561a6cc2f963bb76931
gilbu <gilbertbuix57@gmail.com>
2020-12-01 23:53:56 -0800

    add new canvas

--- src/game_view.js
+++ src/game_view.js
@@ -98,6 +98,34 @@ GameView.prototype.animateGame = function animateGame(time) {
     this.ctx.clearRect(0, 0, this.game.DIM_X, this.game.DIM_Y);
     gameOver.drawGameOver();
     location.reload();
+
+    document.addEventListener("DOMContentLoaded", function () {
+  const page = document.getElementById("page");
+  const canvasEl = document.getElementById("canvas");
+  canvasEl.width = Game.DIM_X;
+  canvasEl.height = Game.DIM_Y;
+  const ctx = canvasEl.getContext("2d");
+  const game = new Game();
+  const gameView = new GameView(game, ctx, canvasEl, page)
+  const title = new Title(ctx, canvasEl);
+  title.drawStartClick();
+  title.drawInstructions();
+  const clickHandler = (e) => {
+    gameView.start(e);
+    canvasEl.removeEventListener("click", clickHandler);
+  }
+
+  const keydownHandler = (e) => {
+    gameView.start(e);
+    if (e.keyCode === 13){
+      page.removeEventListener("keydown", keydownHandler);
+    }
+  };
+
+  canvasEl.addEventListener("click", clickHandler);
+  page.addEventListener("keydown", keydownHandler);
+  title.drawTitle();
+});
   }
   requestAnimationFrame(this.animateGame.bind(this));
 };
